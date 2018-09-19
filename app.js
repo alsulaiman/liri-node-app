c2a7b32d9376a234b944b60b4b3a04610baf3f48
hamad alsulaiman <Hamad@hamadsMacBook-2.local>
2018-09-19 06:05:38 -0400

    update

---
+++ app.js
@@ -0,0 +1,92 @@
+var apiKeys = require('./keys');
+var Twitter = require('twitter');
+var Spotify = require('spotify');
+var Request = require('request');
+var fs = require('fs');
+var Spotify = require('node-spotify-api');
+
+var client = new Twitter({
+    consumer_key: apiKeys.twitterKeys.consumer_key,
+    consumer_secret: apiKeys.twitterKeys.consumer_secret,
+    access_token_key: apiKeys.twitterKeys.access_token_key,
+    access_token_secret: apiKeys.twitterKeys.access_token_secret
+});
+
+var spotify = new Spotify({
+    id: apiKeys.spotifyKeys.id,
+    secret: apiKeys.spotifyKeys.secret
+});
+
+
+var command = process.argv[2];
+var input = process.argv[3];
+
+function commandHandler(commandType, commandSpecifics) {
+    switch (commandType) {
+        case "my-tweets":
+            client.get('statuses/user_timeline', { screen_name: 'sumeet36993623' }, function (error, tweets, response) {
+                if (!error) {
+                    tweets.forEach(function (tweet) {
+                        console.log(tweet.text);
+                    });
+                }
+            });
+            break;
+        case "spotify-this-song":
+            var song = commandSpecifics;
+            if (!song) {
+                console.log('"The Sign" by Ace of Base');
+            } else {
+                spotify.search({ type: 'track', query: song, limit: 5 }, function (err, data) {
+                    if (err) {
+                        console.log('Error occurred: ' + err);
+                        return;
+                    }
+                    console.log(data);
+                    data.tracks.items[0].artists.forEach(function (artist) {
+                        console.log('Artist Name:', artist.name);
+                    });
+                    console.log('Album Name:', data.tracks.items[0].album.name);
+                    console.log('Song Name:', data.tracks.items[0].name);
+                    console.log('Preview Url:', data.tracks.items[0].preview_url);
+                });
+            }
+            break;
+        case "movie-this":
+            var movie = commandSpecifics;
+            if (!movie) {
+                movie = "Mr. Nobody";
+                Request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json&apikey=trilogy", function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var bodyJSON = JSON.parse(body);
+                        for (let prop in bodyJSON) {
+                            console.log(prop + ":", bodyJSON[prop]);
+                        }
+                    }
+                });
+            } else {
+                Request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json&apikey=trilogy", function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var bodyJSON = JSON.parse(body);
+                        for (let prop in bodyJSON) {
+                            console.log(prop + ":", bodyJSON[prop]);
+                        }
+                    }
+                });
+            }
+            break;
+        case "do-what-it-says":
+            var filename = commandSpecifics;
+            fs.readFile("random.txt", "utf8", function (error, data) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    var fileReadCommand = data.toString().trim().split(",");
+                    commandHandler(fileReadCommand[0], fileReadCommand[1]);
+                }
+            });
+            break;
+    }
+}
+
+commandHandler(command, input);
---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'NCjhk9Xn3UdX2G5MujAbEgcIh',
+    consumer_secret: 'o6g9C4HMa8Joh4MYhKW9YVaHb3a3KE6UU3vlpM2MpBxUmqsS1J',
+    access_token_key: '1008570559049842688-zGE1mLvSx5Cw8u4BRxnwKkGSFJf4DT',
+    access_token_secret: 'GDppEdbuOXWeYrmGdiYVrCzjqxBgOcvGqAOx4ZMS9VW9Y'
+};
+
+exports.spotifyKeys = {
+    id: 'd501b676d17d43288d9a1884c8b081f9',
+    secret: '6d2b630b820640ad8f57b96e63389b87'
+};
+
+// module.exports = {
+//     twitterKeys: twitterKeys,
+//     spotifyKeys: spotifyKeys
+// };
